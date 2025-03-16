a9242fef29a20ddd04aca755c89446eebb380647
Artem <you@example.com>
2025-03-16 15:40:54 +0300

    init

---
+++ src/main/java/com/smoke/smokeBack/SmokeBackApplication.java
@@ -0,0 +1,13 @@
+package com.smoke.smokeBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmokeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmokeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smoke/smokeBack/SmokeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoke.smokeBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
